ec96b2ec0046547623ae11ea1ef39444b0742b16
unknown <osamaslam029@gmail.com>
2022-08-03 03:33:30 +0500

    last

--- React Projects/outsource2 Pakistan/src/Components/Services/Services.jsx
+++ React Projects/outsource2 Pakistan/src/Components/Services/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Services.css'
 import cup from "../../Assets/images/Page-1/cup.png"
+import Button from '../Atoms/Button/Button'
 
 export default function Services() {
   return (
@@ -8,7 +9,36 @@ export default function Services() {
         <div className="flex_card">
             <div className="cards">
                 <div className="card">
-                    <img src={cup} alt="" />
+                    <div className="card_image"><img src={cup} alt="error" /></div>
+                    <div className="card_heading">Desktop Publishing <br />Services</div>
+                    <div className="card_btn"><Button text="Learn More"/></div>
+                </div>
+                <div className="card">
+                    <div className="card_image"><img src={cup} alt="error" /></div>
+                    <div className="card_heading">Desktop Publishing <br />Services</div>
+                    <div className="card_btn"><Button text="Learn More"/></div>
+                </div>
+                <div className="card">
+                    <div className="card_image"><img src={cup} alt="error" /></div>
+                    <div className="card_heading">Desktop Publishing <br />Services</div>
+                    <div className="card_btn"><Button text="Learn More"/></div>
+                </div>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <div className="card_image"><img src={cup} alt="error" /></div>
+                    <div className="card_heading">Desktop Publishing <br />Services</div>
+                    <div className="card_btn"><Button text="Learn More"/></div>
+                </div>
+                <div className="card">
+                    <div className="card_image"><img src={cup} alt="error" /></div>
+                    <div className="card_heading">Desktop Publishing <br />Services</div>
+                    <div className="card_btn"><Button text="Learn More"/></div>
+                </div>
+                <div className="card">
+                    <div className="card_image"><img src={cup} alt="error" /></div>
+                    <div className="card_heading">Desktop Publishing <br />Services</div>
+                    <div className="card_btn"><Button text="Learn More"/></div>
                 </div>
             </div>
         </div>
